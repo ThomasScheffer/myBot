8a55c46e5b1106bc6413950c3f1ee59c86d16843
ThomasScheffer <info@tsmedia.nl>
2018-10-25 21:00:33 +0200

    first blizzard api attempt

--- bot.js
+++ bot.js
@@ -1,6 +1,7 @@
 const Discord = require("discord.js");
 const client = new Discord.Client();
 const config = require("./config.json");
+const blizzard = require('blizzard.js').initialize({ apikey: config.blizzard });
 
 client.on("ready", () => {
   console.log("I am ready!");
@@ -8,8 +9,12 @@ client.on("ready", () => {
 
 client.on("message", (message) => {
   if (message.content.startsWith("ping")) {
-    message.channel.send("pong!");
-  }
+    blizzard.wow.character(['profile'], { origin: 'us', realm: 'amanthul', name: 'charni' })
+      .then(response => {
+        console.log(response.data);
+        message.channel.send(response.data);
+      });
+    }
 });
 
 client.login(config.token);

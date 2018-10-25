301a66216dac94c5383a4e0b236f349d7decbe3d
ThomasScheffer <info@tsmedia.nl>
2018-10-25 21:06:22 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,7 +1,6 @@
 const Discord = require("discord.js");
 const client = new Discord.Client();
 const config = require("./config.json");
-const blizzard = require('blizzard.js').initialize({ apikey: config.blizzard });
 
 client.on("ready", () => {
   console.log("I am ready!");
